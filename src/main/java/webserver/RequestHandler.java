08eec087928a9ccd0aed86985e0f73bd3dcf3be2
Hanjinwon <32839363+mynameisjinwon@users.noreply.github.com>
2024-10-05 21:31:00 +0900

    Revert "pom.xml 자잘한 오류 발생해서 몇 줄 삭제"
    This reverts commit 530974dee3a2aa9b1deb35b1e9ce8e897c3fbc0a.

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,8 +1,10 @@
 package webserver;
 
-import java.io.*;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
-import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,7 +13,6 @@ public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
     private Socket connection;
-    private BufferedReader br;
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
@@ -23,14 +24,8 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            br = new BufferedReader(new InputStreamReader(in));
-
-            String url = br.readLine().split(" ")[1];
-            log.debug("url : {}", url);
-
             DataOutputStream dos = new DataOutputStream(out);
-//            byte[] body = "Hello World".getBytes();
-            byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+            byte[] body = "Hello World".getBytes();
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
