23006b16c4ed2ec5f721b63ee47cfe3bd90fb3a2
Hanjinwon <32839363+mynameisjinwon@users.noreply.github.com>
2025-01-10 23:02:19 +0900

    enum 클래스 추가

--- src/main/java/controller/AbstractController.java
+++ src/main/java/controller/AbstractController.java
@@ -5,12 +5,12 @@ import webserver.HttpResponse;
 
 public abstract class AbstractController implements Controller {
 
-    abstract void doPost(HttpRequest request, HttpResponse response);
+    protected abstract void doPost(HttpRequest request, HttpResponse response);
 
-    abstract void doGet(HttpRequest request, HttpResponse response);
+    protected abstract void doGet(HttpRequest request, HttpResponse response);
 
     public void service(HttpRequest request, HttpResponse response) {
-        if ("POST".equals(request.getMethod())) {
+        if (request.getMethod().isPost()) {
             doPost(request, response);
             return;
         }
